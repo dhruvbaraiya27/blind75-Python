d8cb7b6aaae6467205efdafbf6380ec0104bac7f
Dhruv Baraiya <baraiya.d@northeastern.edu>
2025-08-10 16:11:58 -0400

    Create longestPalindromicSubString.py

---
+++ String/longestPalindromicSubString.py
@@ -0,0 +1,19 @@
+class Solution:
+    def expand(self, s:str, left:int, right:int) -> tuple[int , int]:
+        while left >=0 and right < len(s) and s[left]==s[right]:
+            left -=1
+            right +=1
+        return left+1, right-1
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+        start, end= 0, 0
+        for i in range(len(s)):
+            l1,r1 = self.expand(s, i, i)
+            l2,r2 = self.expand(s, i, i+1)
+
+            if r1 - l1 > end -start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+        return s[start: end+1]
